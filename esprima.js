193ec886141bea82b767f1525903baace83af896
Ariya Hidayat <ariya.hidayat@gmail.com>
2013-05-25 23:57:05 -0700

    Remove location filter for the comment array.
    https://code.google.com/p/esprima/issues/detail?id=417
    (cherry picked from commit 11d647929760b24ff8b156022488afad4184d71f)
    
    Conflicts:
    	esprima.js

--- esprima.js
+++ esprima.js
@@ -500,11 +500,10 @@ parseYieldExpression: true, parseAwaitExpression: true
         // (if any) are skipped twice during the lexical analysis.
         // Thus, we need to skip adding a comment if the comment array already
         // handled it.
-        if (extra.comments.length > 0) {
-            if (extra.comments[extra.comments.length - 1].range[1] > start) {
-                return;
-            }
+        if (state.lastCommentStart >= start) {
+            return;
         }
+        state.lastCommentStart = start;
 
         comment = {
             type: type,
@@ -6514,27 +6513,6 @@ parseYieldExpression: true, parseAwaitExpression: true
         peek();
         body = parseProgramElements();
         return markerApply(marker, delegate.createProgram(body));
-    }
-
-    function filterCommentLocation() {
-        var i, entry, comment, comments = [];
-
-        for (i = 0; i < extra.comments.length; ++i) {
-            entry = extra.comments[i];
-            comment = {
-                type: entry.type,
-                value: entry.value
-            };
-            if (extra.range) {
-                comment.range = entry.range;
-            }
-            if (extra.loc) {
-                comment.loc = entry.loc;
-            }
-            comments.push(comment);
-        }
-
-        extra.comments = comments;
     }
 
     // 16 XJS
